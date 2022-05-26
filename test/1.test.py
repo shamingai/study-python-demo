7edaa3116a01f4a0fab76569ff08762416c2eb8a
zhaotong2027 <18210192027@163.com>
2022-05-27 03:28:31 +0800

    format

--- test/1.test.py
+++ test/1.test.py
@@ -6,7 +6,7 @@ elif eval(C) > 0:
     index = 0
     while 1:
         index += 1
-        print(eval(tempStr[index,index+1]), end = "")
+        print(eval(tempStr[index, index + 1]), end="")
         if index == len(tempStr) - 1:
             break
 else:
